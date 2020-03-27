f209c88496391927c867723a2ec4c94ef52edbee
yiyu0x <yiyu0x@icloud.com>
2020-03-27 23:22:31 +0800

    Add toggle in icon

--- bgp.js
+++ bgp.js
@@ -1,20 +1,19 @@
-chrome.webRequest.onBeforeSendHeaders.addListener ((details) => {
-    console.log(JSON.stringify(details))
+var enable = true
+var number = 0
+chrome.webRequest.onBeforeSendHeaders.addListener((details) => {
+    
     let headers = details.requestHeaders
-    let blockingResponse = {}
-
-    let haveReferer = false
-    for (var i = 0, l = headers.length; i < l; i++ ) {
-      if (headers[i].name.toLowerCase() == 'referer') {
-        headers[i].value = 'https://t.co/';
-        haveReferer = true
-      } 
+    let blockingResponse = {} 
+    
+    if (enable) {
+        for (let i = 0, l = headers.length; i < l; i++ ) {
+            if (headers[i].name.toLowerCase() == 'referer') {
+                headers[i].value = 'https://t.co/'
+            } 
+        }
+        headers.push({'name': 'referer', 'value': 'https://t.co/'})
     }
-    headers.push({'name': 'referer', 'value': 'https://t.co/'})
+    blockingResponse.requestHeaders = headers   
+    return blockingResponse 
 
-    blockingResponse.requestHeaders = headers;
-    return blockingResponse;
-  },
-  {urls: ["<all_urls>"]},
-  ['requestHeaders', 'blocking', 'extraHeaders']
-);
+}, {urls: ["<all_urls>"]}, ['requestHeaders', 'blocking', 'extraHeaders'])
